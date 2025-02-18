4e9039d6efe38c39dbb46e4454465f7f15d145c0
unknown <dunjoyebiz2020@gmail.com>
2025-02-18 23:59:14 +0100

    reached a checkpoint

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Crown } from "../utils/assets";
+import { Crown, Night } from "../utils/assets";
 
 const Home = () => {
   return (
@@ -18,7 +18,25 @@ const Home = () => {
         </div>
       </div>
 
-      <div className="main"></div>
+      <div className="main flex flex-col w-full">
+        <div className="header flex  justify-center  flex-col h-[80px] px-[32px] py-[20px] gap-[10px] border-[1px] border-[#CBD5E1]">
+          <div className="flex items-center justify-center">
+            <div className="lightmode flex-1 ">
+              <img src={Night} className="w-[24px] h-[24px]" />
+            </div>
+
+            <h1 className="title flex-1 font-bold text-[30px]">Summarizer</h1>
+          </div>
+        </div>
+
+        {/* the programs to be imported here */}
+        <div className="chat flex items-center justify-center px-[96px] py-[32px] gap-[24px]">
+         <div className="userinput flex flex-col justify-end w-[808px] h-[78px] gap-[12px] ">
+            <p className="font-bold">You</p>
+            <p className=" flex w-2/5 rounded-[24px] p-[12px] gap-[10px] bg-[#F8FAFC]"> do android really dream</p>
+        </div>   
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/utils/assets.js
+++ src/components/utils/assets.js
@@ -1,3 +1,7 @@
 import cr from "./../../assets/crownLogo.svg";
+import n from "./../../assets/night.svg";
+import d from "./../../assets/day.svg";
 
 export const Crown = cr; 
+export const Night = n;
+export const Day = d;
