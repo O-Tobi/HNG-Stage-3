0a765b70cc35e10d8dc6e53271d5c13b5c1b9202
unknown <dunjoyebiz2020@gmail.com>
2025-02-19 01:56:01 +0100

    done with the homepage

--- src/components/utils/assets.js
+++ src/components/utils/assets.js
@@ -3,9 +3,11 @@ import n from "./../../assets/night.svg";
 import d from "./../../assets/day.svg";
 import ai from "./../../assets/aiAvatar.svg";
 import ak from "./../../assets/avatarKyoshi.svg";
+import sb from "./../../assets/submitButton.svg";
 
 export const Crown = cr; 
 export const Night = n;
 export const Day = d;
 export const AiAvatar = ai;
 export const UserAvatar = ak; 
+export const SubmitButton = sb;
