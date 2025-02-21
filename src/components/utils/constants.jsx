99285a308dd7641d784e4458136906db5a95a89d
unknown <dunjoyebiz2020@gmail.com>
2025-02-21 11:20:42 +0100

    reached a checkpoint

--- src/components/utils/constants.jsx
+++ src/components/utils/constants.jsx
@@ -2,10 +2,8 @@ export const languages = [
   { code: "fr", name: "French" },
   { code: "en", name: "English" },
   { code: "es", name: "Spanish" },
-  { code: "de", name: "German" },
-  { code: "zh", name: "Chinese" },
-  { code: "ja", name: "Japanese" },
+  { code: "tr", name: "Turkish" },
+  { code: "pt", name: "Portuguese" },
   { code: "ru", name: "Russian" },
   { code: "ar", name: "Arabic" },
-  { code: "hi", name: "Hindi" },
 ];
